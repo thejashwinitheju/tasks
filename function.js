6ab2836d9287c24a00f441ed81d12936e9288ef1
thejashwinitheju <thejashwinitheju912@gmail.com>
2022-11-07 23:24:39 +0530

    funtion

---
+++ function.js
@@ -0,0 +1,33 @@
+
+//Object:- group of propeties
+// group of key vlaue paire in one entity
+//group of date and funtion
+
+let emp[
+    {id:101,name:theju}
+    {id:102,name:dev}
+]
+console.log(id);
+
+// funtion:- gtroup of statement for spacif task
+
+function emp(a,b){
+    console.log(a+b);
+}
+let result=emp(10,20)
+console.log(result);
+
+//oops:-object orieanted progeam
+//class:-class is a tamplet to creat an object once object is created memory loaction is allocated
+
+class Employe{
+    bank_name="HDFC";
+    barnch(){
+console.log("banglore");
+    }}
+    let a1=new Employe();
+    console.log(a1.bank_name);
+    a1.barnch();
+
+    //Event:- 
+
